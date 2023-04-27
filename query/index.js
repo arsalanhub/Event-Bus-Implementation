c42f272605eb95509fadfb7c7246c344a98c04f1
Arsalan <arsalanmohd237@gmail.com>
2023-04-27 17:36:55 +0530

    Integrate frontend with Query service

--- query/index.js
+++ query/index.js
@@ -4,12 +4,24 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/posts", (req, res) => {
+const posts = {};
 
+app.get("/posts", (req, res) => {
+    res.send(posts);
 })
 
 app.post("/events", (req, res) => {
-
+    const { type, data } = req.body;
+    if(type === 'PostCreated') {
+        const { id, title } = data;
+        posts[id] = { id, title, comments: [] };
+    } 
+    if(type === 'CommentCreated') {
+        const { id, content, postId } = data;
+        const post = posts[postId];
+        post.comments.push({ id, content });
+    }
+    res.send({ msg: "Event Received" });
 })
 
 app.listen(4002, () => {
