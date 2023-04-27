1398c94349c3d9205e59ce1a8afd267e330d8fdb
Arsalan <arsalanmohd237@gmail.com>
2023-04-27 15:50:12 +0530

    Post & Comment service sending req to event bus

--- comments/index.js
+++ comments/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const axios = require("axios");
 const { randomBytes } = require("crypto");
 const app = express();
 app.use(express.json());
@@ -11,12 +12,20 @@ app.get("/posts/:id/comments", (req, res) => {
     res.send(commentsByPostId[req.params.id] || []);
 })
 
-app.post("/posts/:id/comments", (req, res) => {
+app.post("/posts/:id/comments", async (req, res) => {
     const commentId = randomBytes(4).toString("hex");
     const { content } = req.body;
     const comments = commentsByPostId[req.params.id] || [];
     comments.push({ id: commentId, content });
     commentsByPostId[req.params.id] = comments;
+    await axios.post("http://localhost:4005/events", {
+        type: "CommentCreated",
+        data: {
+            id: commentId,
+            content,
+            postId: req.params.id
+        }
+    })
     res.status(201).send(comments);
 })
 
--- event-bus/index.js
+++ event-bus/index.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const cors = require("cors");
+const axios = require("axios");
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.post("/events", async (req, res) => {
     const event = req.body;
-    await axios.post("http://localhost:4000/events", event);
-    await axios.post("http://localhost:4001/events", event);
-    await axios.post("http://localhost:4002/events", event);
+    console.log(req.body);
+    // await axios.post("http://localhost:4000/events", event)
+    // await axios.post("http://localhost:4001/events", event);
+    // await axios.post("http://localhost:4002/events", event);
 
     res.send({ status: "OK" });
 })
