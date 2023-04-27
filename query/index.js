ed3af7cda5b3491555619bcf6136a2964f0d4e16
Arsalan <arsalanmohd237@gmail.com>
2023-04-27 16:15:27 +0530

    Query service added

---
+++ query/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/posts", (req, res) => {
+
+})
+
+app.post("/events", (req, res) => {
+
+})
+
+app.listen(4002, () => {
+    console.log("Listening on 4002");
+})
